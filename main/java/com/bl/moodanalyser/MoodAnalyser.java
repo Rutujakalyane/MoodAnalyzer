9465ef873e3af983a04fa6382a8ddc0e129829b7
Rutujakalyane <kalyanerutuja7@gmail.com>
2020-04-23 19:49:21 +0530

    ADDED:[TC 1.2]Should return HAPPY,If mood is Happy

--- main/java/com/bl/moodanalyser/MoodAnalyser.java
+++ main/java/com/bl/moodanalyser/MoodAnalyser.java
@@ -1,4 +1,27 @@
 package com.bl.moodanalyser;
 
 public class MoodAnalyser {
+    private String message;
+
+    public MoodAnalyser() {
+        public MoodAnalyser(String message)
+        {
+            if(message == null){
+                this.message=null;
+            }else if(message.contains("happy")){
+                this.message="HAPPY";}
+            else
+                this.message="SAD";
+        }
+        public String analyseMood() {
+            try {
+                if (message.contains("sad"))
+                    return "SAD";
+            } catch (NullPointerException e) {
+                return "HAPPY";
+            }
+            return message;
+        }
+
+    }
 }
