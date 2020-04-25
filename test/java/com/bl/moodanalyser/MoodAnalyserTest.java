e8a2e2a5b5ee42afd624f70f0d8ef117ae44e9d7
Rutujakalyane <kalyanerutuja7@gmail.com>
2020-04-25 13:18:47 +0530

    ADDED:[TC 3.2]Empty mood should throw exception

--- test/java/com/bl/moodanalyser/MoodAnalyserTest.java
+++ test/java/com/bl/moodanalyser/MoodAnalyserTest.java
@@ -2,15 +2,13 @@ package com.bl.moodanalyser;
 import org.junit.Assert;
 import org.junit.Test;
 
-
-    public class MoodAnalyserTest {
+public class MoodAnalyserTest {
         @Test
         public void givenMessage_WhenSad_Should_ReturnSad() throws MoodAnalyserException {
             MoodAnalyser analyzer = new MoodAnalyser("this sad is message");
             String mood = analyzer.analyseMood();
             Assert.assertEquals("SAD",mood);
         }
-
         @Test
         public void givenMessage_WhenAny_ShouldReturnHappy() throws MoodAnalyserException {
             MoodAnalyser analyzer = new MoodAnalyser("I am in any mood");
@@ -18,15 +16,12 @@ import org.junit.Test;
             Assert.assertEquals("HAPPY",mood);
 
         }
-
         @Test
         public void givenMessage_WhenNull_ShouldReturnHappy() throws MoodAnalyserException {
             MoodAnalyser analyzer = new MoodAnalyser("NULL");
             String  mood = analyzer.analyseMood();
             Assert.assertEquals("HAPPY",mood);
         }
-
-       
         @Test
         public void givenMessage_WhenEmpty_ShouldReturnMoodAnalyserException() {
             MoodAnalyser analyser = new MoodAnalyser("");
@@ -35,8 +30,6 @@ import org.junit.Test;
             } catch (MoodAnalyserException e) {
                 Assert.assertEquals(MoodAnalyserException.exceptionType.ENTERED_EMPTY,e.type);
             }
-
-
         }
     }
 
